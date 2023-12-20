b0a9309b48b2c2a11a80c143299210c31728f5ec
Crush Bate <50185023+batec2@users.noreply.github.com>
2023-12-20 15:40:44 -0700

    added overlay using electron-overlay-windw

--- Project/main.js
+++ Project/main.js
@@ -1,15 +1,31 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require ('electron');
+const { OverlayController, OVERLAY_WINDOW_OPTS } = require ('electron-overlay-window');
+
+//app.disableHardwareAcceleration()
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    ...OVERLAY_WINDOW_OPTS
+  });
+  win.loadFile('index.html');
 
-  win.loadFile('index.html')
+  // NOTE: if you close Dev Tools overlay window will lose transparency
+  win.webContents.openDevTools({ mode: 'detach', activate: false })
+  //https://github.com/SnosMe/electron-overlay-window/blob/master/src/demo/electron-demo.ts
+  //for demo of overlay
+  OverlayController.attachByTitle(
+    win,
+    process.platform === 'darwin' ? 'Untitled' : 'RuneLite',
+    { hasTitleBarOnMac: true }
+  )
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 })
-
