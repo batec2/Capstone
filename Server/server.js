cef8be1bc5b8027175379742eeab3144fb19ae95
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-02-21 20:12:09 -0700

    removed worker thread from ml server

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,49 @@
-import {Worker,MessageChannel} from 'worker_threads';
+import { Worker, MessageChannel } from "worker_threads";
 import { Server } from "socket.io";
-const modelWorker = new Worker('./utils/tfModel.js');
+import { predict, warmUpModel } from "./utils/tfModel.js";
+import { splitResult } from "./utils/tfTools.js";
+// const modelWorker = new Worker("./utils/tfModel.js");
 
-
-const io = new Server(3003, {
-    maxHttpBufferSize: 1e8
+const io = new Server(3005, {
+  maxHttpBufferSize: 1e8,
 });
 
-console.log('go');
+console.log("go");
 
-io.on('connection', socket => {
-    console.log(socket.id);
-    socket.on('image',image=>{
-        const frame = {data:Array.from(image.data),width:image.width,height:image.height};
-        modelWorker.postMessage(frame);
-    });
+const model = await warmUpModel();
 
-    /**
-     * listens for a message from worker thread, and sends the resutls to
-     * the front end to be rendered
-     */
-    modelWorker.on('message',(results)=>{
-        console.log(results);
-        console.log(results.length);
-        if(results){
-            socket.emit('bounding',results);
-        }
-    });
+io.on("connection", async (socket) => {
+  console.log(socket.id);
+  socket.on("image", (image) => {
+    const frame = {
+      data: Array.from(image.data),
+      width: image.width,
+      height: image.height,
+    };
+    // modelWorker.postMessage(frame);
+    const results = predict(frame, model);
+    // console.log(results);
+    if (results) {
+      const slicedResults = splitResult(results);
+      socket.emit("bounding", slicedResults);
+    }
+  });
+  //   socket.emit("bounding", results);
+  /**
+   * listens for a message from worker thread, and sends the resutls to
+   * the front end to be rendered
+   */
+  //   modelWorker.on("message", (results) => {
+  //     console.log(results);
+  //     // console.log(results.length);
+  //     if (results) {
+  //       socket.emit("bounding", results);
+  //     }
+  //   });
 });
 
-io.on('disconnect',()=>{
-    console.log('disconnected');
+io.on("disconnect", () => {
+  console.log("disconnected");
 });
 
 // export LD_LIBRARY_PATH=/home/crush/.local/lib/python3.10/site-packages/nvidia/cudnn/lib/libcudnn.so.8:$LD_LIBRARY_PATH
--- Server/utils/tfTools.js
+++ Server/utils/tfTools.js
@@ -1,71 +1,96 @@
-import * as tf from '@tensorflow/tfjs-node-gpu';
-import * as fs from 'node:fs';
+import * as tf from "@tensorflow/tfjs-node-gpu";
+import * as fs from "node:fs";
 
 /**
- * 
- * @param {*} imageData 
- * @param {*} rank 
- * @returns 
+ *
+ * @param {*} imageData
+ * @param {*} rank
+ * @returns
  */
-function getTensorFromImageData(imageData,rank){
-    return tf.browser.fromPixels(imageData,rank);
-}
+export const getTensorFromImageData = (imageData, rank) => {
+  return tf.browser.fromPixels(imageData, rank);
+};
 
 /**
- * 
- * @param {*} location 
- * @param {*} rank 
- * @returns 
+ *
+ * @param {*} location
+ * @param {*} rank
+ * @returns
  */
-function getTensorFromFile(location,rank){
-    const image = imageToTensor(location,rank);
-    return resizeTensor(image);
-}
+export const getTensorFromFile = (location, rank) => {
+  const image = imageToTensor(location, rank);
+  return resizeTensor(image);
+};
 
 /**
- * 
- * @param {*} location 
- * @param {*} rank 
- * @returns 
+ *
+ * @param {*} location
+ * @param {*} rank
+ * @returns
  */
-function imageToTensor(location,rank){
-    const image = fs.readFileSync(location);
-    return tf.node.decodePng(image,rank);
-}
-
-function nonMaxSuppression(box,score){
-
-}
+export const imageToTensor = (location, rank) => {
+  const image = fs.readFileSync(location);
+  return tf.node.decodePng(image, rank);
+};
 
 /**
  * Saves a tensor to a png file
- * @param {*} tensor 
+ * @param {*} tensor
  */
-function saveImage(tensor){
-    tf.node.encodePng(tensor).then((image)=>{
-        fs.writeFileSync('redone.png',image);
+export const saveImage = (tensor) => {
+  tf.node
+    .encodePng(tensor)
+    .then((image) => {
+      fs.writeFileSync("redone.png", image);
     })
-    .catch((err)=>{
-        console.log('Error at saveImage toPixel');
-        console.log(err);
+    .catch((err) => {
+      console.log("Error at saveImage toPixel");
+      console.log(err);
     });
-}
+};
 
 /**
  * Resizes image tensor by padding tensor to the shape of 864x864
- * @param {*} image 
- * @returns 
+ * @param {*} image
+ * @returns
  */
-function resizeTensor(image){
-    const height = image.shape[1];
-    const width = image.shape[0];
-    let img = image.pad([[0,864-width],[0,864-height],[0,0]])//pads image to 864,864
-        .div(255.0)//normalizing tensor to floats between 0-1                  
-        .transpose([2, 0, 1])//no idea, something about turning rows into columns?
-        .expandDims(0);//it expands dimensions?
-        console.log(img);
+export const resizeTensor = (image) => {
+  const height = image.shape[1];
+  const width = image.shape[0];
+  const padX = 864 - width;
+  const padY = 864 - height;
+  if (padX < 0 || padY < 0) {
+    return null;
+  }
+  let img = image
+    .pad([
+      [0, padX],
+      [0, padY],
+      [0, 0],
+    ]) //pads image to 864,864
+    .div(255.0) //normalizing tensor to floats between 0-1
+    .transpose([2, 0, 1]) //no idea, something about turning rows into columns?
+    .expandDims(0); //it expands dimensions?
 
-    return img;//[1,3,864,864] rank of 4 (4d array) to match model requirements
-}
+  return img; //[1,3,864,864] rank of 4 (4d array) to match model requirements
+};
 
-export {getTensorFromFile,getTensorFromImageData,resizeTensor,saveImage};
+const nonMaxSuppression = (results) => {};
+
+export const splitResult = (results) => {
+  //gets top left xy, and bottom right xy
+  const bounding = results.map((result) => [
+    result[0] - result[2] / 2,
+    result[1] - result[3] / 2,
+    result[2],
+    result[3],
+  ]);
+  const scores = results.map((result) => result[4]);
+  const dectectionClass = results.map((result) => result[5]);
+
+  return {
+    bounding: bounding,
+    scores: scores,
+    detectionClass: dectectionClass,
+  };
+};
